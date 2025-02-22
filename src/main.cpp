0a3063d1296612f0286b2e9592f734b84d54bac9
Grégoire Maire <g.maire118@gmail.com>
2025-02-22 21:11:56 +0100

    draw array and camera control

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,33 @@
 #include <cmath>
 
 #include "gfx/gfx.h"
+#include "ui/ui.h"
 #include "cube.h"
 
 // TODO
-// camera class
 // chunk class
 // render cubes have fun
 
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-const float Z_NEAR = 0.1f;
-const float Z_FAR = 100.0f;
-const float FOV_DEGREES = 45.0f;
-const float aspectRatio = (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT;
-
-extern Window window; // todo does this work ? check with input
+extern Window window;
 
 int main(int argc, char** argv) {
   glEnable(GL_DEPTH_TEST);
 
   Camera camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
-  Shader shader("resources/shaders/default.vert", "resources/shaders/default.frag");
+  Shader shader("resources/shaders/default.vert",
+                "resources/shaders/default.frag");
   Texture tex("resources/textures/wooden_container.jpg");
+  // UI ui(window);
 
-  VAO cubeVAO; 
+  VAO cubeVAO;
   VBO cubeVBO(GL_ARRAY_BUFFER, false);
   cubeVBO.buffer(vertices, sizeof(vertices));
   cubeVAO.attr(cubeVBO, 0, 3, GL_FLOAT, 5 * sizeof(float), 0);
   cubeVAO.attr(cubeVBO, 1, 2, GL_FLOAT, 5 * sizeof(float), 3 * sizeof(float));
+
+  shader.use();
+  glm::mat4 p = camera.get_perspective_matrix();
+  shader.uniform_mat4("p", p);
 
   while (!glfwWindowShouldClose(window)) {
     window.begin_frame();
@@ -39,20 +38,25 @@ int main(int argc, char** argv) {
     glClearColor(0.4f, 0.3f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Build shader uniforms
+    tex.bind();
     shader.use();
     shader.uniform_texture2D("tex", tex, 0);
-    glm::mat4 m = glm::rotate(glm::mat4(1.0f), glm::radians(-40.0f), glm::vec3(1.0f, 1.0f, 1.0f)); // rotate object
     glm::mat4 v = camera.get_view_matrix();
-    glm::mat4 p = camera.get_perspective_matrix();
-    ;
-    shader.uniform_mat4("m", m);
     shader.uniform_mat4("v", v);
-    shader.uniform_mat4("p", p);
 
-    // Draw
     cubeVAO.bind();
-    glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
+    for (int i = 0; i < 16; i++) {
+      for (int j = 0; j < 16; j++) {
+        for (int k = 0; k < 16; k++) {
+          glm::mat4 m = glm::translate(glm::mat4(1.0f),
+                                       glm::vec3((float)i, (float)j, (float)k));
+          shader.uniform_mat4("m", m);
+          glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float));
+        }
+      }
+    }
+
+    // ui.render();
 
     window.end_frame();
   }
